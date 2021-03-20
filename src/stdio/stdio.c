1ea510f24aa3486a846b0deed5af8fdf25eef8a2
Jean Carlos <jeaanca.santos@gmail.com>
2021-03-20 18:14:33 -0300

    Removed headers file

--- src/stdio/stdio.c
+++
@@ -1,5 +0,0 @@
-#include "../terminal.c"
-
-void printf(const char* str) {
-    terminal_write(str);
-}
--- src/stdio/stdio.h
+++
@@ -1,8 +0,0 @@
-#ifndef __STDIO
-#define __STDIO
-
-void printf(const char* str) {
-    terminal_write(str);
-}
-
-#endif
